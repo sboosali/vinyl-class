92cfa2235c2527516baf0479e55ab577cda6c58f
Spiros Boosalis <samboosalis@gmail.com>
2016-06-03 19:34:23 +0300

    class Record

--- sources/Vinyl/Class.hs
+++ sources/Vinyl/Class.hs
@@ -1,8 +1,45 @@
+{-# LANGUAGE DataKinds, KindSignatures, PolyKinds #-}
+
 {-| typeclasses for @vinyl@ records, @newtype@s thereoef,
 and custom representations.
 
 -}
 module Vinyl.Class where
-import Vinyl.Class.Extra
+-- import Vinyl.Class.Extra
+
+import Data.Vinyl
 
 --TODO final? initial? other?
+--------------------------------------------------------------------------------
+
+{-| intial encoding: constructing heterogeneous sequences.
+
+same kind as 'Rec':
+
+@
+record :: (k -> *) -> [k] -> *
+@
+
+same associativity/precendence as (':&'):
+
+@
+infixr 7 `cons`
+@
+
+-}
+class Record (record :: (k -> *) -> [k] -> *) where
+
+  nil :: record f '[]
+
+  cons :: f a -> record f as -> record f (a ': as)
+  infixr 7 `cons`
+
+{-|@
+nil = 'RNil'
+cons = (':&'')
+@-}
+instance Record Rec where
+  nil = RNil
+  cons = (:&)
+
+--------------------------------------------------------------------------------
--- sources/Vinyl/Class/Example.hs
+++ sources/Vinyl/Class/Example.hs
@@ -1,5 +1,12 @@
+{-# LANGUAGE DataKinds, NoMonomorphismRestriction #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Vinyl.Class.Example where
-import Vinyl.Class()
+import Vinyl.Class
+
+import Data.Vinyl
+import Data.Vinyl.Functor
+
+--------------------------------------------------------------------------------
 
 {-|
 @
@@ -9,4 +16,18 @@ stack build && stack exec -- vinyl-class-example
 main :: IO ()
 main = do
  putStrLn ""
- print $ "Vinyl.Class"
+ print $ rec_vinyl
+
+--------------------------------------------------------------------------------
+
+{-| Inferred:
+
+@
+rec :: (Record record, Num i) => record Identity [Bool,i,String]
+@
+
+-}
+rec = Identity False `cons` Identity 0 `cons` Identity "" `cons` nil
+
+rec_vinyl :: Rec Identity [Bool,Int,String]
+rec_vinyl = rec
