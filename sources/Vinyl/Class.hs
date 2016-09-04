efbf6850836b562d704248592b3ffb66054eac93
Sam Boosalis <samboosalis@gmail.com>
2016-09-04 17:01:59 +0300

    FinalRecord

--- sources/Vinyl/Class.hs
+++ sources/Vinyl/Class.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE DataKinds, KindSignatures, PolyKinds, RankNTypes, ExistentialQuantification #-}
-{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE ExplicitNamespaces, RecordWildCards #-}
 
 {-| typeclasses for @vinyl@ records, @newtype@s thereoef,
 and custom representations.
@@ -15,6 +15,12 @@ import Data.Vinyl.TypeLevel
 --------------------------------------------------------------------------------
 
 {-| intial encoding: constructing heterogeneous sequences.
+
+@
+record f as
+@
+
+TODO the functor @f@ is a "parameter", the types @as@ are the "index".
 
 same kind as 'Rec':
 
@@ -54,18 +60,26 @@ instance InitialRecord Rec where
 
 the method is like a product of handlers.
 
+TODO "type index" versus "type param": the param is constant, indices vary?
+
 -}
 class FinalRecord (record :: (k -> *) -> [k] -> *) where
   record
-   ::                           b
-   -> (forall x. f x -> b -> b)
+   :: forall (f :: k -> *) (as :: [k]) (output :: [k] -> *). ()
+   => (                                 output '[]      )
+   -> (forall x xs. f x -> output xs -> output (x ': xs))
    -> record f as
 
-{-|@
-record nil' cons' = \case
-    'RNil'      -> nil
-    (f ':&' fs) -> f `cons'` fs
-@-}
+{-old
+:: forall (f :: k -> *) (as :: [k]) (output :: [k] -> *). ()
+-> (record f as -> output as)
+-}
+
+-- {-|@
+-- record nil' cons' = \case
+--     'RNil'      -> nil
+--     (f ':&' fs) -> f `cons'` fs
+-- @-}
 -- instance FinalRecord Rec where
 --   record nil' cons' = \case
 --       RNil      -> nil'
@@ -75,20 +89,41 @@ record nil' cons' = \case
 record = 'R'
 @-}
 instance FinalRecord R where
-  record = R
+ record = R
+
+{-old
+record nil' cons' = R nil' cons'
+-}
 
 --------------------------------------------------------------------------------
 
 {- | a concrete "final record" ('FinalRecord').
 
+e.g. mapping
+
+@
+@
+
+e.g. folding
+
+@
+(All Num as) => R f as -> Const Int as
+@
 
 -}
-data R f as = forall b. R --NOTE -XExistentialQuantification
- { _nil  :: b
- , _cons :: forall a. (a ∈ as) => f a -> b -> b -- TODO
+data R f as = forall (output :: [*] -> *). R --NOTE -XExistentialQuantification
+ { _nil  :: output '[]
+ , _cons :: (forall x xs. f x -> output xs -> output (x ': xs))
  }
 
+-- how does an R "hold" {f a}'s, like a Rec does? or does it?
+
 {-old
+
+{ _nil  :: b
+, _cons :: forall a. (a ∈ as) => f a -> b -> b -- TODO
+}
+
 , _cons :: forall a. f a -> R f as -> b
 , _cons :: forall a. f a -> b -> b
 , _cons :: forall a. (a ∈ as) => f a -> b -> b
@@ -96,6 +131,22 @@ data R f as = forall b. R --NOTE -XExistentialQuantification
 -}
 
 --------------------------------------------------------------------------------
+
+r2rec :: R f as -> Rec f as
+r2rec R{..} = undefined
+
+{-old
+\case
+ RNil -> _nil
+ (f :& fs) -> _cons f fs
+
+record RNil (:&)
+-}
+
+rec2r :: Rec f as -> R f as
+rec2r = \case
+  RNil -> undefined
+  (f :& fs) -> undefined
 
 {-
 rappend
@@ -118,3 +169,13 @@ rappend (x :& xs) ys = x :& (xs `rappend` ys)
 --   nil'  = undefined
 --   cons' = undefined
 --   --  if the first is nil, then return the second
+
+--------------------------------------------------------------------------------
+
+{-notes
+
+(a + b) -> c  ~  (a -> c) * (b -> c)
+
+Rec f as -> output as  ~  R f as
+
+-}
